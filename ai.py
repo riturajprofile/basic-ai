c12b9b51a3c70b3fa16744071d321325f49ac02d
rituraj <riturajprofile@gmail.com>
2025-10-16 14:52:56 +0000

    adding history to ai

--- ai.py
+++ ai.py
@@ -17,13 +17,16 @@ provider = OpenAIProvider(
 model = OpenAIChatModel("gpt-4o", provider=provider)
 
 # Create agent
-agent = Agent(model)
+agent = Agent(model,system_prompt="you are a helpful assistant.give very short answers.answer in funny way with jokes")
+
+message_history=[]
 
 while True:
     user_input = input("You: ")
     if user_input.lower() in ["exit", "quit"]:
         break
-    response = agent.run_sync(user_input)
+    response = agent.run_sync(user_input, message_history=message_history)
+    message_history=response.all_messages()
     print("Agent:", response.output)
 # Run synchronously
 # result = agent.run_sync("hello i am rituraj")
